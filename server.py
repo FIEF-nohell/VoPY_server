8f535f32e59222f46459b00da0ab75c219178285
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-22 20:07:34 +0200

    server class

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 import socket
 from threading import Thread
 
+class Server:
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((host, port))
+        self.server.listen(5)
+        self.clients = {}
 
 if __name__ == "__main__":
-
+    server = Server("0.0.0.0", 12345)
+    server.run()
