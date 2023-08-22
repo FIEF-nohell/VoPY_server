c668705f2ccfe869f0c8e24b7e9c4da8433bc088
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-22 20:08:16 +0200

    broadcast and client handler

--- server.py
+++ server.py
@@ -8,6 +8,22 @@ class Server:
         self.server.listen(5)
         self.clients = {}
 
+    def broadcast(self, data, source):
+        for client, addr in self.clients.items():
+            if client != source:
+                try:
+                    client.send(data)
+                except:
+                    pass
+
+    def handle_client(self, client):
+        while True:
+            try:
+                data = client.recv(1024)
+                self.broadcast(data, client)
+            except:
+                pass
+
 if __name__ == "__main__":
     server = Server("0.0.0.0", 12345)
     server.run()
